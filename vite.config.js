e316d5798b54b45774f8df5066b4873d97cf82b9
QuanLeHoang <lehoangquan19082012@gmail.com>
2026-08-10 11:53:43 +0700

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/test/',
-  plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          animation: ['gsap', 'aos'],
-          vendor: ['react', 'react-dom']
+    base: '/quoc-khanh-website/',
+
+    plugins: [react()],
+
+    build: {
+        cssCodeSplit: true,
+
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    animation: ['gsap', 'aos'],
+                    vendor: ['react', 'react-dom']
+                }
+            }
         }
-      }
     }
-  }
 });
